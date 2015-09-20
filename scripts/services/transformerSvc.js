df0c859961a49879d9c9b4c5795d539746483aba
Atul <atulanand94@gmail.com>
2015-09-20 15:11:15 +0530

    formatted indeed

--- scripts/services/transformerSvc.js
+++ scripts/services/transformerSvc.js
@@ -1,18 +1,18 @@
 var _ = require('lodash');
 
-var transformerSvc = function () {
+var transformerSvc = function() {
 
-  var formatLinkedin = function (results) {
+  var formatLinkedin = function(results) {
     var formatted = {};
 
-    return formatted
+    return formatted;
   };
 
-  var formatGithub = function (results) {
-    var formatted = _.map(results, function (eachResult) {
+  var formatGithub = function(results) {
+    var formatted = _.map(results, function(eachResult) {
       return {
-        f_name: (''+eachResult.userlistinfo_value).split(' ')[0],
-        l_name: (''+eachResult.userlistinfo_value).split(' ')[1] || '',
+        f_name: ('' + eachResult.userlistinfo_value).split(' ')[0],
+        l_name: ('' + eachResult.userlistinfo_value).split(' ')[1] || '',
         skill: '',
         location: eachResult.userlistmeta_value,
         profile_url: eachResult.userlistinfo_link,
@@ -25,14 +25,31 @@ var transformerSvc = function () {
         stage1_detials: '',
         stage2_details: '',
         stage3_details: ''
-      }
+      };
     });
-    return formatted
+    return formatted;
   };
-  var formatIndeed = function (results) {
-    var formatted = {};
 
-    return formatted
+  var formatIndeed = function(results) {
+    var formatted = _.map(results, function(eachResult) {
+      return {
+        f_name: ('' + eachResult.name).split(' ')[0],
+        l_name: ('' + eachResult.name).split(' ')[1] || '',
+        skill: '',
+        location: eachResult.location,
+        profile_url: eachResult.userlistinfo_link,
+        work_ex: eachResult.experience,
+        image_url: eachResult.image,
+        misc_url: '',
+        search_source: 'Indeed',
+        misc_details: 'Education :'+eachResult.education,
+        curr_stage: '',
+        stage1_detials: '',
+        stage2_details: '',
+        stage3_details: ''
+      };
+    });
+    return formatted;
   };
 
   return {
