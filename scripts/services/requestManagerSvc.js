0778237072b8402c64e7ed4705385880ef7be717
Atul <atulanand94@gmail.com>
2015-10-07 23:33:47 +0530

    pagination

--- scripts/services/requestManagerSvc.js
+++ scripts/services/requestManagerSvc.js
@@ -10,26 +10,29 @@ var requestManager = function() {
     var promiseArray = [];
 
     _.forEach(allSiteConfigs, function(eachSiteConfigObject) {
-      var defer = q.defer();
       var siteName = _.keys(eachSiteConfigObject)[0];
       var siteJsonConfig = JSON.parse(eachSiteConfigObject[siteName]);
       siteJsonConfig.url = urlManager.urlConstructor(siteJsonConfig.url, substitutions);
-      console.log('Scrapping : ', siteName, ' Url:', siteJsonConfig.url);
+      console.log('Scrapping : ', siteName, ' Urls:', siteJsonConfig.url);
 
-      request.get({
-        url: siteJsonConfig.url,
-        headers: {
-          'Content-Type:': 'text/plain'
-        }
-      }, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          defer.resolve(scrapperX.scrape(body, siteJsonConfig));
-        } else {
-          console.log('Error for ', siteName, response.statusCode, response);
-          defer.reject(error);
-        }
+      _.forEach(siteJsonConfig.url,function(eachUrl){
+        var defer = q.defer();
+        request.get({
+          url: eachUrl,
+          headers: {
+            'Content-Type:': 'text/plain'
+          }
+        }, function(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            defer.resolve(scrapperX.scrape(body, siteJsonConfig));
+          } else {
+            console.log('Error for ', siteName, response.statusCode, response);
+            defer.reject(error);
+          }
+        });
+        promiseArray.push(defer.promise);
       });
-      promiseArray.push(defer.promise);
+
     });
     return q.all(promiseArray);
   };
